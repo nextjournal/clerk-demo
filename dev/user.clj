ad93f857f3e5e55207c40bdd0d54b6b3de484874
Martin Kavalar <mk@katercalling.com>
2021-10-10 21:10:00 +0200

    Update to use latest Clerk, demo static build

--- dev/user.clj
+++ dev/user.clj
@@ -13,7 +13,11 @@
   (def filewatcher
     (beholder/watch #(clerk/file-event %) "notebooks" "src"))
 
+  ;; and stop it again
   (beholder/stop filewatcher)
+
+  ;; open clerk
+  (browse/browse-url (str "http://localhost:" port))
 
   ;; or call `clerk/show!` explicitly
   (clerk/show! "notebooks/introduction.clj")
@@ -22,6 +26,7 @@
   (clerk/show! "notebooks/elements.clj")
   (clerk/show! "notebooks/pagination.clj")
   (clerk/show! "notebooks/how_clerk_works.clj")
+  (clerk/show! "notebooks/tablecloth.clj")
 
   (clerk/show! "notebooks/viewers/html.clj")
   (clerk/show! "notebooks/viewers/markdown.clj")
@@ -30,6 +35,10 @@
   (clerk/show! "notebooks/viewers/tex.clj")
   (clerk/show! "notebooks/viewers/vega.clj")
 
-  (browse/browse-url (str "http://localhost:" port))
-
+  ;; produce a static build of some notebooks
+  ;; ⚠️ warning: this inlines the results.
+  ;; This means it's currently only for small results.
+  (clerk/build-static-app! {:paths (concat
+                                    ["notebooks/rule_30.clj"]
+                                    (map #(str "notebooks/viewers/" % ".clj") '[html markdown plotly tex vega]))})
   )
