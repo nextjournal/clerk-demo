f6a658ef99f51ced1f786f508aa5357d7acf0cb1
Jack Rusher <jack@rusher.com>
2021-11-16 21:13:03 +0100

    remove tablecloth example for now

--- dev/user.clj
+++ dev/user.clj
@@ -15,19 +15,31 @@
   (browse/browse-url (str "http://localhost:" port))
 
   ;; or call `clerk/show!` explicitly
-  (clerk/show! "notebooks/rule_30.clj")
-  (clerk/show! "notebooks/viewer_api.clj")
-  (clerk/show! "notebooks/how_clerk_works.clj")
-  (clerk/show! "notebooks/pagination.clj")
-  (clerk/show! "notebooks/tablecloth.clj")
-  (clerk/show! "notebooks/semantic.clj")
-
-  (clerk/show! "notebooks/viewers/html.clj")
-  (clerk/show! "notebooks/viewers/markdown.clj")
-  (clerk/show! "notebooks/viewers/plotly.clj")
+  (clerk/show! "notebooks/introduction.clj") ; combine with "hello" and make a nice intro
+  (clerk/show! "notebooks/viewer_api.clj") ; combine these two
+  ;; make a notebook for simple "data science" stuff, maybe using kixi?
+  ;; * read a CSV
+  ;; * take some summary stats
+  ;; * graph a few things
+  ;; could use setosa, or maybe grab a more interesting data set?
   (clerk/show! "notebooks/viewers/table.clj")
-  (clerk/show! "notebooks/viewers/tex.clj")
-  (clerk/show! "notebooks/viewers/vega.clj")
+
+  (clerk/show! "notebooks/rule_30.clj")
+  (clerk/show! "notebooks/semantic.clj")
+  
+  ;; if you want to know more of the details about how it works and
+  ;; why it's reasonably fast
+  (clerk/show! "notebooks/how_clerk_works.clj")
+
+  ;; TODO move to tests?
+  (clerk/show! "notebooks/pagination.clj")
+
+  ;; remove these in favor of the intro itself
+  ;; (clerk/show! "notebooks/viewers/html.clj")
+  ;; (clerk/show! "notebooks/viewers/markdown.clj")
+  ;; (clerk/show! "notebooks/viewers/tex.clj")
+  ;; (clerk/show! "notebooks/viewers/plotly.clj")
+  ;; (clerk/show! "notebooks/viewers/vega.clj")
 
   (clerk/show! "src/arslonga/core.clj")
   ;; produce a static app
