bbf0624a637ee7765cb5867fed61b01113ef0dd6
Jack Rusher <jack@rusher.com>
2021-11-19 13:13:38 +0100

    Add sicmutils demo

--- dev/user.clj
+++ dev/user.clj
@@ -5,46 +5,38 @@
   ;; start without file watcher, open browser when started
   (clerk/serve! {:browse? true})
 
-  ;; start with file watcher
+  ;; start with file watcher for these sub-directory paths
   (clerk/serve! {:watch-paths ["notebooks" "src"]})
 
-  ;; start with file watcher and show filter function to enable notebook pinning
+  ;; start with file watcher and a `show-filter-fn` function to watch
+  ;; a subset of notebooks
   (clerk/serve! {:watch-paths ["notebooks" "src"] :show-filter-fn #(clojure.string/starts-with? % "notebooks")})
 
-  ;; open clerk
-  (browse/browse-url (str "http://localhost:" port))
-
   ;; or call `clerk/show!` explicitly
-  (clerk/show! "notebooks/introduction.clj") ; combine with "hello" and make a nice intro
-  (clerk/show! "notebooks/viewer_api.clj") ; combine these two
+  (clerk/show! "notebooks/introduction.clj") ; combine with "hello" and "pagination" to make a nice intro
+  (clerk/show! "notebooks/pagination.clj")
+  (clerk/show! "notebooks/viewer_api.clj") ; expand or combine with intro?
+
   ;; make a notebook for simple "data science" stuff, maybe using kixi?
   ;; * read a CSV
   ;; * take some summary stats
   ;; * graph a few things
   ;; could use setosa, or maybe grab a more interesting data set?
   (clerk/show! "notebooks/data_science.clj")
+
+  (clerk/show! "notebooks/sicmutils.clj")
+
+  ;; done
   (clerk/show! "notebooks/rule_30.clj")
   (clerk/show! "notebooks/semantic.clj")
   
-  ;; if you want to know more of the details about how it works and
-  ;; why it's reasonably fast
+  ;; TODO If you would like more details about how Clerk works, here's a
+  ;; notebook with some implementation details.
   (clerk/show! "notebooks/how_clerk_works.clj")
 
-  ;; TODO move to tests?
-  (clerk/show! "notebooks/pagination.clj")
-
-  ;; remove these in favor of the intro itself
-  ;; (clerk/show! "notebooks/viewers/html.clj")
-  ;; (clerk/show! "notebooks/viewers/markdown.clj")
-  ;; (clerk/show! "notebooks/viewers/tex.clj")
-  ;; (clerk/show! "notebooks/viewers/plotly.clj")
-  ;; (clerk/show! "notebooks/viewers/vega.clj")
-
-  (clerk/show! "src/arslonga/core.clj")
   ;; produce a static app
   (clerk/build-static-app! {:paths (mapv #(str "notebooks/" % ".clj")
-                                         '[rule_30 viewer_api how_clerk_works pagination tablecloth
-                                           viewers/html viewers/markdown viewers/plotly viewers/tex viewers/vega])})
+                                         '[introduction data_science rule_30 semantic])})
 
   )
 
