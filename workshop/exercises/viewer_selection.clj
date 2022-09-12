f1ec5920b5f984169106b80b8a55344310cc9f0d
Martin Kavalar <martin@nextjournal.com>
2022-09-12 16:09:15 +0200

    Add viewer selection exercise

---
+++ workshop/exercises/viewer_selection.clj
@@ -0,0 +1,20 @@
+;; # Viewer Selection
+
+(ns exercises.viewer-selection
+  (:require [exercises.visibility :as vis-ex]
+            [nextjournal.clerk :as clerk]))
+
+;; To select a viewer, you can use functions or metadata. In what
+;; situations would you reach for which one? Explore the differences
+;; also at the REPL.
+
+;; ## Explicit Function
+
+(def table-explicit-function
+  (clerk/table vis-ex/life-expectancy))
+
+;; ## Metadata
+
+^{::clerk/viewer clerk/table}
+(def table-metadata
+  vis-ex/life-expectancy)
