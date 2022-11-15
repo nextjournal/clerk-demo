d08d7bc8e9a6b272cdf95f537cd7425b471e312e
Andrea Amantini <lo.zampino@gmail.com>
2022-11-15 16:17:13 +0100

    Fix Spirograph

--- notebooks/osc_spirograph.clj
+++ notebooks/osc_spirograph.clj
@@ -1,13 +1,10 @@
 ;; # ꩜ An OSC _fourieristic_ Spirograph
-;; _This short text shows how to use an [OSC](https://en.wikipedia.org/wiki/Open_Sound_Control)
-;; driven controller running on your phone to interact with vector graphic animations in a Clerk notebook. OSC is generally employed in live multimedia
-;; devices and sound synthesizers, but as [remarked a while ago by Joe Armstrong](https://joearms.github.io/published/2016-01-28-A-Badass-Way-To-Connect-Programs-Together.html)
-;; its properties make it an interesting choice for exchanging data across machines in a broader range of applications._
 ^{:nextjournal.clerk/visibility {:code :fold}}
 (ns osc-spirograph
   (:require [nextjournal.clerk :as clerk]
             [clojure.java.io :as io]
-            [nextjournal.clerk.viewer :as v])
+            [nextjournal.clerk.viewer :as v]
+            [nextjournal.clerk.render :as-alias clerk.render])
   (:import (com.illposed.osc ConsoleEchoServer OSCMessageListener OSCMessageEvent OSCMessage OSCBundle)
            (com.illposed.osc.messageselector JavaRegexAddressMessageSelector)
            (com.illposed.osc.transport OSCPortOut)
@@ -15,15 +12,19 @@
            (java.net InetSocketAddress)
            (javax.imageio ImageIO)
            (java.util List ArrayList)))
+;; _This short text shows how to use an [OSC](https://en.wikipedia.org/wiki/Open_Sound_Control)
+;; driven controller running on your phone to interact with vector graphic animations in a Clerk notebook. OSC is generally employed in live multimedia
+;; devices and sound synthesizers, but as [remarked a while ago by Joe Armstrong](https://joearms.github.io/published/2016-01-28-A-Badass-Way-To-Connect-Programs-Together.html)
+;; its properties make it an interesting choice for exchanging data across machines in a broader range of applications._
 
-^{::clerk/visibility {:code :hide :result :hide}}
+^{::clerk/visibility {:code :fold :result :hide}}
 (def client-model-sync
   ;; This viewer is used to sync models between clojure values and those on the client side
   {:transform-fn (comp v/mark-presented (v/update-val (comp deref deref ::clerk/var-from-def)))
    :render-fn    '(fn [val]
                     (defonce model (atom nil))
                     (v/html
-                     [v/inspect-paginated
+                     [clerk.render/inspect
                       (-> (swap! model
                                  (partial merge-with (fn [old new] (if (vector? old) (mapv merge old new) new)))
                                  val)
@@ -47,82 +48,82 @@
 (def spirograph-viewer
   {:render-fn '(fn [_]
                  (v/html
-                   [v/with-d3-require {:package "two.js@0.7.13"}
-                    (fn [Two]
-                      (reagent/with-let
-                        [Vector (.-Vector Two) Line (.-Line Two) Group (.-Group Two)
-                         world-matrix (.. Two -Utils -getComputedMatrix)
-                         R 200 MAXV 1000 time-scale 0.09 frequency-factor (* 2 js/Math.PI 0.025)
-                         arm-color ["#f43f5e" "#65a30d" "#4338ca"] ;; [ r , g , b ]
-                         phasor-group (fn [drawing parent {:keys [amplitude color]}]
-                                        (let [G (doto (Group.)
-                                                  (j/assoc! :position
-                                                            (j/get-in parent [:children 0 :vertices 1]
-                                                                      (Vector. (/ (.-width drawing) 2)
-                                                                               (/ (.-height drawing) 2)))))]
-                                          (.add parent G)
-                                          (.add G (doto (Line. 0.0 0.0 (* amplitude R) 0.0)
-                                                    (j/assoc! :linewidth 7)
-                                                    (j/assoc! :stroke color)
-                                                    (j/assoc! :cap "round")))
-                                          G))
-                         build-phasors (fn [{:as m :keys [drawing]}]
-                                         (update m :phasors (fn [phasors]
-                                                              (->> phasors
-                                                                   (transduce (map-indexed (fn [i ph] (assoc ph :color (arm-color i))))
-                                                                              (fn
-                                                                                ([] {:phasors [] :parent-group (.-scene drawing)})
-                                                                                ([ret] (:phasors ret))
-                                                                                ([{:as acc :keys [parent-group]} params]
-                                                                                 (let [g (phasor-group drawing parent-group params)]
-                                                                                   (-> acc
-                                                                                       (update :phasors conj (-> params (assoc :group g) (dissoc :color)))
-                                                                                       (assoc :parent-group g))))))))))
-                         update-phasor! (fn [{:keys [amplitude frequency group]} dt]
-                                          (when group
-                                            (j/assoc-in! group [:children 0 :vertices 1 :x] (* amplitude R))
-                                            (j/update! group :rotation + (* frequency-factor frequency dt))))
-                         build-curve (fn [{:as m :keys [drawing]}]
-                                       (assoc m :curve
-                                                (doto (.makeCurve drawing)
-                                                  (j/assoc! :closed false)
-                                                  (j/assoc! :stroke "#5b21b6")
-                                                  (j/assoc! :linewidth 5)
-                                                  (j/assoc! :opacity 0.8)
-                                                  .noFill)))
-                         pen-position (fn [{:keys [phasors]}]
-                                        (let [{:keys [amplitude group]} (last phasors)]
-                                          (.copy (Vector.)
-                                                 (-> group world-matrix (.multiply (* amplitude R) 0.0 1.0)))))
-                         ->color (fn [{:keys [phasors]}]
-                                   (let [[r g b] (map (comp js/Math.floor (partial * 200) :amplitude) phasors)]
-                                     (str "rgb(" r "," g "," b ")")))
-                         update-curve! (fn [{:as model :keys [drawing mode curve]} dt]
-                                         (when curve
-                                           (let [vxs (.-vertices curve) size (.-length vxs)]
-                                             (case (or mode 0)
-                                               0            ;; spirograph
-                                               (.push vxs (pen-position model))
-                                               1            ;; fourier
-                                               (doto vxs
-                                                 (.push (j/assoc! (pen-position model) :x (/ (.-width drawing) 2)))
-                                                 (.forEach (fn [p] (j/update! p :x - dt))))
-                                               nil)
-                                             (when (< MAXV size) (.splice vxs 0 (- size MAXV)))
-                                             (j/assoc! curve :stroke (->color model)))))
-                         apply-model (fn [{:as model :keys [clean? curve drawing phasors]} dt]
-                                       (doseq [rot phasors] (update-phasor! rot dt))
-                                       (js/requestAnimationFrame #(update-curve! model dt)) ;; draw curve at next tick
-                                       (when clean? (.remove drawing curve))
-                                       (cond-> model clean? build-curve))
-                         update! (fn [_frames dt] (swap! model apply-model (* time-scale dt)))
-                         refn (fn [el]
-                                (when (and el (not (:drawing @model)))
-                                  (let [drawing (Two. (j/obj :type (.. Two -Types -svg) :autostart true :fitted true))]
-                                    (.appendTo drawing el)
-                                    (.bind drawing "update" update!)
-                                    (swap! model #(-> % (assoc :drawing drawing) build-phasors build-curve)))))]
-                        [:div {:ref refn :style {:width "100%" :height "800px"}}]))]))})
+                  [v/with-d3-require {:package "two.js@0.7.13"}
+                   (fn [Two]
+                     (reagent/with-let
+                      [Vector (.-Vector Two) Line (.-Line Two) Group (.-Group Two)
+                       world-matrix (.. Two -Utils -getComputedMatrix)
+                       R 200 MAXV 1000 time-scale 0.09 frequency-factor (* 2 js/Math.PI 0.025)
+                       arm-color ["#f43f5e" "#65a30d" "#4338ca"] ;; [ r , g , b ]
+                       phasor-group (fn [drawing parent {:keys [amplitude color]}]
+                                      (let [G (doto (Group.)
+                                                (j/assoc! :position
+                                                          (j/get-in parent [:children 0 :vertices 1]
+                                                                    (Vector. (/ (.-width drawing) 2)
+                                                                             (/ (.-height drawing) 2)))))]
+                                        (.add parent G)
+                                        (.add G (doto (Line. 0.0 0.0 (* amplitude R) 0.0)
+                                                  (j/assoc! :linewidth 7)
+                                                  (j/assoc! :stroke color)
+                                                  (j/assoc! :cap "round")))
+                                        G))
+                       build-phasors (fn [{:as m :keys [drawing]}]
+                                       (update m :phasors (fn [phasors]
+                                                            (->> phasors
+                                                                 (transduce (map-indexed (fn [i ph] (assoc ph :color (arm-color i))))
+                                                                            (fn
+                                                                              ([] {:phasors [] :parent-group (.-scene drawing)})
+                                                                              ([ret] (:phasors ret))
+                                                                              ([{:as acc :keys [parent-group]} params]
+                                                                               (let [g (phasor-group drawing parent-group params)]
+                                                                                 (-> acc
+                                                                                     (update :phasors conj (-> params (assoc :group g) (dissoc :color)))
+                                                                                     (assoc :parent-group g))))))))))
+                       update-phasor! (fn [{:keys [amplitude frequency group]} dt]
+                                        (when group
+                                          (j/assoc-in! group [:children 0 :vertices 1 :x] (* amplitude R))
+                                          (j/update! group :rotation + (* frequency-factor frequency dt))))
+                       build-curve (fn [{:as m :keys [drawing]}]
+                                     (assoc m :curve
+                                            (doto (.makeCurve drawing)
+                                              (j/assoc! :closed false)
+                                              (j/assoc! :stroke "#5b21b6")
+                                              (j/assoc! :linewidth 5)
+                                              (j/assoc! :opacity 0.8)
+                                              .noFill)))
+                       pen-position (fn [{:keys [phasors]}]
+                                      (let [{:keys [amplitude group]} (last phasors)]
+                                        (.copy (Vector.)
+                                               (-> group world-matrix (.multiply (* amplitude R) 0.0 1.0)))))
+                       ->color (fn [{:keys [phasors]}]
+                                 (let [[r g b] (map (comp js/Math.floor (partial * 200) :amplitude) phasors)]
+                                   (str "rgb(" r "," g "," b ")")))
+                       update-curve! (fn [{:as model :keys [drawing mode curve]} dt]
+                                       (when curve
+                                         (let [vxs (.-vertices curve) size (.-length vxs)]
+                                           (case (or mode 0)
+                                             0            ;; spirograph
+                                             (.push vxs (pen-position model))
+                                             1            ;; fourier
+                                             (doto vxs
+                                               (.push (j/assoc! (pen-position model) :x (/ (.-width drawing) 2)))
+                                               (.forEach (fn [p] (j/update! p :x - dt))))
+                                             nil)
+                                           (when (< MAXV size) (.splice vxs 0 (- size MAXV)))
+                                           (j/assoc! curve :stroke (->color model)))))
+                       apply-model (fn [{:as model :keys [clean? curve drawing phasors]} dt]
+                                     (doseq [rot phasors] (update-phasor! rot dt))
+                                     (js/requestAnimationFrame #(update-curve! model dt)) ;; draw curve at next tick
+                                     (when clean? (.remove drawing curve))
+                                     (cond-> model clean? build-curve))
+                       update! (fn [_frames dt] (swap! model apply-model (* time-scale dt)))
+                       refn (fn [el]
+                              (when (and el (not (:drawing @model)))
+                                (let [drawing (Two. (j/obj :type (.. Two -Types -svg) :autostart true :fitted true))]
+                                  (.appendTo drawing el)
+                                  (.bind drawing "update" update!)
+                                  (swap! model #(-> % (assoc :drawing drawing) build-phasors build-curve)))))]
+                      [:div {:ref refn :style {:width "100%" :height "800px"}}]))]))})
 
 ^{::clerk/width :full ::clerk/visibility {:code :hide} ::clerk/viewer spirograph-viewer}
 (Object.)
@@ -155,7 +156,7 @@
 ;; Well also sync back to the device every time we compute the notebook.
 (defonce osc-in
   (proxy [ConsoleEchoServer]
-         [(InetSocketAddress. "0.0.0.0" 6669) (LoggerFactory/getLogger ConsoleEchoServer)]
+    [(InetSocketAddress. "0.0.0.0" 6669) (LoggerFactory/getLogger ConsoleEchoServer)]
     (start []
       (proxy-super start)
       (.. this
@@ -167,7 +168,7 @@
                            (let [{:keys [path value]} (osc->map (.getMessage event))]
                              (update-model! #(assoc-in % path value))))))))))
 
-(defonce osc-out (OSCPortOut. (InetSocketAddress. "10.33.8.65" 7777)))
+(defonce osc-out (OSCPortOut. (InetSocketAddress. "192.168.1.53" 7777)))
 
 (defn sync-osc [{:keys [phasors mode]}]
   (.send osc-out
@@ -192,9 +193,9 @@
 ;; to which I refer the reader to further explore the implications of Fourier analysis with digital signal processing.
 ;; My article should definitely expand to also contain some sound, probably using overtone. Suggestions anyone? [@lo_zampino](https://twitter.com/lo_zampino)
 
-^{::clerk/visibility {:code :hide :result :hide}}
+{::clerk/visibility {:code :hide :result :hide}}
 (comment
-  (clerk/serve! {:port 7777})
+  (clerk/serve! {:port 7788})
   (clerk/clear-cache!)
 
   @model
@@ -220,6 +221,15 @@
   @model
   (do
     (reset! model
+            {:mode 0,
+             :phasors [{:amplitude 0.4, :frequency -0.22}
+                       {:amplitude 0.24, :frequency 0.22}
+                       {:amplitude 0.81, :frequency 0.65}]}
+            #_
+                    {:mode 0,
+                     :phasors [{:amplitude 1.0, :frequency 0.41}
+                               {:amplitude 0.64, :frequency -0.41}
+                               {:amplitude 0.17, :frequency -0.54}]}
             #_{:mode    0,
                :phasors [{:amplitude 0.4, :frequency 0.2}
                          {:amplitude 1.0, :frequency -0.2}
@@ -235,11 +245,11 @@
                          {:amplitude 0.83, :frequency 0.2}
                          {:amplitude 1.0, :frequency 0.35}]}
 
-
-            {:mode    0,
-             :phasors [{:amplitude 0.41, :frequency 0.46}
-                       {:amplitude 0.46, :frequency -0.44}
-                       {:amplitude 1.0, :frequency -0.45}]}
+            #_
+                    {:mode    0,
+                     :phasors [{:amplitude 0.41, :frequency 0.46}
+                               {:amplitude 0.46, :frequency -0.44}
+                               {:amplitude 1.0, :frequency -0.45}]}
 
             #_{:mode    0
                :phasors [{:amplitude 0.57, :frequency 0.39}
