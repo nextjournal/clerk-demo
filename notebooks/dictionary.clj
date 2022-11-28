50afe9199c8b2a3488e4fffb29d529b76d47cfcb
Andrea Amantini <lo.zampino@gmail.com>
2022-11-28 16:45:41 +0100

    Hide input-viewer from results

--- notebooks/dictionary.clj
+++ notebooks/dictionary.clj
@@ -27,8 +27,6 @@
 
 #_(reset! text-state "")
 
-{::clerk/visibility {:result :show}}
-
 (def text-input
   (assoc v/viewer-eval-viewer
          :render-fn
@@ -37,7 +35,10 @@
                      :class "px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:ring w-full"
                      :on-input #(swap! text-state (constantly (.. % -target -value)))}])))
 
+{::clerk/visibility {:result :show}}
+
 (clerk/html [:div [:span {:class "font-bold"} "Type three or more letters of a regex!"]])
+
 ^{:nextjournal.clerk/viewer text-input}
 `text-state
 
